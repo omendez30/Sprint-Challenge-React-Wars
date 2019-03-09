531ebdaf9313d77bedd36362a95073ba5651b8aa
omendez30 <omendez9@outlook.com>
2019-03-09 12:59:07 -0500

    created characters  and character list

---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./StarWars.css";
+import Character from "./Character";
+
+function CharacterList(props) {
+  return (
+    <div>
+      {props.characters.map(character => (
+        <Character character={character} key={character.created} />
+      ))}
+    </div>
+  );
+}
+export default CharacterList;
